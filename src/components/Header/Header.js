f416e1e0cb4032595fe7ef7d14a7b384a8e6d0e7
Jessica RODRIGUES <jessik0204@hotmail.com>
2019-03-07 09:31:01 -0300

    correçao logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,10 @@ import './index.css';
 export default function Header() {
   return (
     <div className="header pure-menu pure-menu-horizontal pure-menu-fixed">
-      <a href="/"><img className="logo" src={require("./img/logo.png")} /></a>
+      <a href="/">
+        <img className="logo" src={require("./img/logo.png")} />
+      </a>
       <h4 className="label">Agenda de gentileza</h4>
     </div>
-  )
+  );
 }
